6f520409440c2dc4161c1ddc55e78bdb9cef840d
Andrij Jesmjenejev <antrex@email.cz>
2023-12-10 22:05:07 +0100

    resolved tasks for strings1

---
+++ Solutions/CodingBat/hello_name.py
@@ -0,0 +1,20 @@
+"""
+
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+hello_name('Bob') → 'Hello Bob!'
+hello_name('Alice') → 'Hello Alice!'
+hello_name('X') → 'Hello X!'
+"""
+
+
+# def hello_name(name):
+#     return f"Hello {name}!"
+
+def hello_name(name):
+    return "Hello " + name + "!"
+
+#first function doesn't work in CodingBat, because of the python version
+
+
