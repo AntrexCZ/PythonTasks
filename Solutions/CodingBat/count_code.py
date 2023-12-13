f9b1c63cee557be1f3c8bfcfd21c55abb2c77345
Andrij Jesmjenejev <antrex@email.cz>
2023-12-13 21:39:58 +0100

    final strings tasks

--- Solutions/CodingBat/count_code.py
+++ Solutions/CodingBat/count_code.py
@@ -13,8 +13,8 @@ x = "cozexxcopecode"
 
 def count_code(str):
     count = 0
-    for i in range(len(str)):
-        if str[i:i+4] == "code" or (str[i:i+2] == "co" and str[i+3:i+4] == "e"):
+    for i in range(len(str) - 3):
+        if str[i:i+2] == "co" and str[i+3:i+4] == "e":
             count += 1
     return count
 
