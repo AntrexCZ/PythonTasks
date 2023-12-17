c7eee739bb300c69de4fbb5ea424900d39586629
Andrij Jesmjenejev <antrex@email.cz>
2023-12-17 21:41:14 +0100

    Logic tasks

---
+++ Solutions/CodingBat/Logic/make_chocolate.py
@@ -0,0 +1,22 @@
+"""
+
+We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each).
+Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+
+
+make_chocolate(4, 1, 9) → 4
+make_chocolate(4, 1, 10) → -1
+make_chocolate(4, 1, 7) → 2
+"""
+x,y,z = (6, 2, 7)
+
+def make_chocolate(small, big, goal):
+    if goal % 5 <= small and goal - big * 5 <= small and goal >= big * 5:
+        return goal - big * 5
+    elif goal % 5 <= small and goal - big * 5 <= small and goal <= big * 5:
+        return goal - (big-1) * 5 #adjusted the result, but dont think it is correct one.
+    else:
+        return -1
+
+# Check this task with Vlad, doesn't look correctly., work in progress :)
+print(make_chocolate(x,y,z))
